24fa9592e3a39e6a6ca75fcacd1749a67ca17f7b
Kmt2001 <krishna.tiwari@knoldus.com>
2023-02-20 12:21:43 +0530

    auto trigger task

--- Assignment.java
+++ Assignment.java
@@ -8,7 +8,8 @@ public class Assignment {
         Assignment obj = new Assignment();
         int Array[] = {5, 10, 40, 30, 20};
         obj.orderOfElements(Array);
-        System.out.println("changes");
+        System.out.println("changes made successfully");
+        
     }
 
 
